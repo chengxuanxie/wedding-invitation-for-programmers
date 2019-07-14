6e31cdd8fb95806405ca2647f518b8f9920e6635
root <root@vultr.guest>
2019-07-14 15:14:04 +0000

    adding install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-workdir=`pwd`
+workdir=$(cd $(dirname $0); pwd)
+echo ${workdir}
+git pull origin
+yarn install
+yarn run build
+echo 'build ok'
+sh /opt/xcx/apache-tomcat-7.0.94/bin/shutdown.sh
+sh /opt/xcx/apache-tomcat-7.0.94/bin/startup.sh
+cp -rf ${workdir}/dist/* /opt/xcx/apache-tomcat-7.0.94/webapps/ROOT/
+echo 'start ok'
